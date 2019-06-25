cd610ae57de54fd69bd977a77ad14803771674f8
LiamColeE <Liamedlinger@gmail.com>
2019-06-24 21:50:08 -0600

    added rotation to the objects

--- JS/contact.js
+++ JS/contact.js
@@ -1,6 +1,7 @@
 class FallingObject{
     constructor(text,ypos){
         this.xpos = 0;
+        this.rotation = Math.random() * 360;
         this.textNode = document.createTextNode(text.value);
         this.element = document.createElement("p");
         this.element.append(this.textNode);
@@ -8,16 +9,30 @@ class FallingObject{
         this.element.style.position = "absolute";
         this.element.style.left = `${ypos}px`;
         this.element.style.top = "0px";
-        this.element.style.width = "250px";
-        setInterval(this.fall.bind(this),10);
+        this.element.style.color = "black";
+        this.element.style.textAlign = "center";
+        this.interval = setInterval(this.fall.bind(this),5);
     }
 
     fall(){
-        this.xpos+=1;
+        this.xpos++;
+        if(this.xpos >= screenHeight-30){
+            clearInterval(this.interval);
+        }
         this.element.style.top = `${this.xpos}px`;
+        this.rotate();
+    }
+
+    rotate(){
+        this.rotation++;
+        if(this.rotation>360){
+            this.rotation = 0;
+        }
+        this.element.style.transform = `rotate(${this.rotation}deg)`;
     }
 }
 
+let screenHeight = window.innerHeight;
 let sendBtn = document.querySelector("#send-btn");
 let name = document.querySelector("#full-name");
 let email = document.querySelector("#email");
@@ -26,7 +41,7 @@ let body = document.querySelector("#contact-body");
 let items = [name,email,body];
 
 sendBtn.addEventListener("click", () => {
-    setInterval(spawnItem,1000);
+    setInterval(spawnItem, 1000);
 });
 
 function spawnItem(){
