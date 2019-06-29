dc4356cfd272626644c113e4eea7780ca6b1e835
LiamColeE <Liamedlinger@gmail.com>
2019-06-29 11:57:53 -0600

    formattiong and shrunk original size of the snake to 2

--- JS/contact.js
+++ JS/contact.js
@@ -1,5 +1,5 @@
-class FallingObject{
-    constructor(text,ypos){
+class FallingObject {
+    constructor(text, ypos) {
         this.xpos = 0;
         this.rotation = Math.random() * 360;
         this.textNode = document.createTextNode(text.value);
@@ -8,21 +8,21 @@ class FallingObject{
         document.querySelector("body").appendChild(this.element);
         this.element.classList.add("fallingObj");
         this.element.style.left = `${ypos}px`
-        this.interval = setInterval(this.fall.bind(this),5);
+        this.interval = setInterval(this.fall.bind(this), 5);
     }
 
-    fall(){
+    fall() {
         this.xpos++;
-        if(this.xpos >= screenHeight-10){
+        if (this.xpos >= screenHeight - 10) {
             clearInterval(this.interval);
         }
         this.element.style.top = `${this.xpos}px`;
         this.rotate();
     }
 
-    rotate(){
+    rotate() {
         this.rotation++;
-        if(this.rotation>360){
+        if (this.rotation > 360) {
             this.rotation = 0;
         }
         this.element.style.transform = `rotate(${this.rotation}deg)`;
@@ -37,32 +37,32 @@ let body = document.querySelector("#contact-body");
 
 let alert = document.querySelector(".hidden");
 
-let items = [name,email,body];
+let items = [name, email, body];
 
 sendBtn.addEventListener("click", () => {
-    
+
     alert.style.visibility = "visible"
     setInterval(spawnItem, 500);
 });
 
-function spawnItem(){
+function spawnItem() {
     console.log(window.innerWidth);
-    new FallingObject(items[Math.floor(Math.random() * 3)], Math.floor(Math.random()*window.innerWidth));
+    new FallingObject(items[Math.floor(Math.random() * 3)], Math.floor(Math.random() * window.innerWidth));
 }
 
 ////
 //snake game
 ////
-class Snake{
-    constructor(block){
-        this.pos = [[100,0],[80,0],[60,0],[40,0],[20,0],[0,0]];
+class Snake {
+    constructor(block) {
+        this.pos = [[20, 0], [0, 0]];
         this.bodySections = [];
         this.direction = 2;
         this.update = 4;
         this.addblock = block;
         console.log(block);
 
-        this.pos.forEach((element,index) => {
+        this.pos.forEach((element, index) => {
             this.bodySections[index] = document.createElement("div");
             this.bodySections[index].classList.add("snk-bdy");
             document.querySelector("body").appendChild(this.bodySections[index]);
@@ -72,16 +72,16 @@ class Snake{
         });
 
         document.onkeypress = (e) => {
-            if(e.key == "w" && this.direction != 2){
+            if (e.key == "w" && this.direction != 2) {
                 this.update = 0;
             }
-            else if(e.key == "d" && this.direction != 3){
+            else if (e.key == "d" && this.direction != 3) {
                 this.update = 1;
             }
-            else if(e.key =="s" && this.direction != 0){
+            else if (e.key == "s" && this.direction != 0) {
                 this.update = 2;
             }
-            else if(e.key == "a" && this.direction != 1){
+            else if (e.key == "a" && this.direction != 1) {
                 this.update = 3;
             }
         };
@@ -89,39 +89,39 @@ class Snake{
         setInterval(this.move.bind(this), 200);
     }
 
-    move(){
+    move() {
         //update direction
-        if(this.update == 0){
+        if (this.update == 0) {
             this.direction = 0;
             this.update = 4;
         }
-        else if(this.update == 1){
+        else if (this.update == 1) {
             this.direction = 1;
             this.update = 4;
         }
-        else if(this.update == 2){
+        else if (this.update == 2) {
             this.direction = 2;
             this.update = 4;
         }
-        else if(this.update == 3){
+        else if (this.update == 3) {
             this.direction = 3;
             this.update = 4;
         }
 
-        if(this.direction == 0){
-            this.pos.unshift([this.pos[0][0],this.pos[0][1] - 20]);
+        if (this.direction == 0) {
+            this.pos.unshift([this.pos[0][0], this.pos[0][1] - 20]);
         }
-        else if(this.direction == 1){
-            this.pos.unshift([this.pos[0][0] + 20,this.pos[0][1]]);
+        else if (this.direction == 1) {
+            this.pos.unshift([this.pos[0][0] + 20, this.pos[0][1]]);
         }
-        else if(this.direction == 2){
-            this.pos.unshift([this.pos[0][0],this.pos[0][1] + 20]);
+        else if (this.direction == 2) {
+            this.pos.unshift([this.pos[0][0], this.pos[0][1] + 20]);
         }
-        else if(this.direction == 3){
-            this.pos.unshift([this.pos[0][0] - 20,this.pos[0][1]]);
+        else if (this.direction == 3) {
+            this.pos.unshift([this.pos[0][0] - 20, this.pos[0][1]]);
         }
 
-        if(this.addblock.pos[0] == this.pos[0][0] && this.addblock.pos[1] == this.pos[0][1]){
+        if (this.addblock.pos[0] == this.pos[0][0] && this.addblock.pos[1] == this.pos[0][1]) {
             this.bodySections.push(document.createElement("div"));
             this.bodySections[this.bodySections.length - 1].classList.add("snk-bdy");
             document.querySelector("body").appendChild(this.bodySections[this.bodySections.length - 1]);
@@ -131,26 +131,26 @@ class Snake{
 
             this.addblock.respawn();
         }
-        else{
+        else {
             this.pos.pop();
         }
 
 
-        this.bodySections.forEach((element,index) => {
+        this.bodySections.forEach((element, index) => {
             this.bodySections[index].style.left = `${this.pos[index][0]}px`;
             this.bodySections[index].style.top = `${this.pos[index][1]}px`;
         });
     }
 
-    
+
 }
 
-class addBlock{
-    constructor(){
+class addBlock {
+    constructor() {
         this.pos = [0, 0];
-        this.pos[0] = (Math.random()*window.innerWidth);
+        this.pos[0] = (Math.random() * window.innerWidth);
         this.pos[0] -= this.pos[0] % 20;
-        this.pos[1] = (Math.random()*window.innerHeight);
+        this.pos[1] = (Math.random() * window.innerHeight);
         this.pos[1] -= this.pos[1] % 20;
         this.display = document.createElement("div");
         this.display.classList.add("snk-bdy");
@@ -161,10 +161,10 @@ class addBlock{
         this.display.style.top = `${this.pos[1]}px`;
     }
 
-    respawn(){
-        this.pos[0] = (Math.random()*window.innerWidth);
+    respawn() {
+        this.pos[0] = (Math.random() * window.innerWidth);
         this.pos[0] -= this.pos[0] % 20;
-        this.pos[1] = (Math.random()*window.innerHeight);
+        this.pos[1] = (Math.random() * window.innerHeight);
         this.pos[1] -= this.pos[1] % 20;
 
         this.display.style.left = `${this.pos[0]}px`;
